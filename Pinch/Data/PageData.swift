da9ebe86c354a56e261d4c33c93ebbc8f61be812
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-06 22:55:34 +0800

    Displaying thumbnails on DrawerView and allowing selection of photo to display

---
+++ Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by justin.bitancor on 10/6/24.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id:1, imageName: "magazine-front-cover"),
+    Page(id:2, imageName: "magazine-back-cover")
+]
