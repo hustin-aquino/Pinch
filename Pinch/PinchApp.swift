40ff6c3b86cc7d4cb1fdf56742e897a8940d8894
hustin-aquino <justinmosesbitancor@gmail.com>
2024-10-06 21:19:09 +0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by justin.bitancor on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
